a05823100b140417ad767266a2f8aa87cbca5c21
dhamo7010 <mesh@sixteen87.com>
2023-04-12 19:16:44 +0530

    config chnages

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ passport.deserializeUser(function (user, done) {
 //Start: Load model, controller, helper, and route
 //var model = require('./app/models/mongo/index')(mongoose);
 //var model = require('./app/models/mysql/index')(Sequelize, sequelizeDB);
-var controllers = require('./app/controllers/index')(model);
+//var controllers = require('./app/controllers/index')(model);
 //require('./routes/index.js')(app, model, controllers);
 global.helper = require('./app/helpers/helpers.js');
 
