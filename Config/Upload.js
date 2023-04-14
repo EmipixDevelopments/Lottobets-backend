f5eb0e79ca1e7360a5542e5d39b57b907e29571f
dhamo7010 <mesh@sixteen87.com>
2023-04-14 11:52:29 +0530

    new pool structure add

--- Config/Upload.js
+++ Config/Upload.js
@@ -1,7 +1,7 @@
 // MySql Connection Data
 
 module.exports = {
-	PROFILE:'/var/www/html/kiosk_cng/public/profile/',
-	PRIVATE_KEY:'/var/www/html/kiosk_cng/privatekey.json',
-	NPV:'/var/www/html/kiosk_cng/public/npv/'
+	PROFILE:'/public_html/Lottobets-backend/public/profile/',
+	PRIVATE_KEY:'/public_html/Lottobets-backend/privatekey.json',
+	NPV:'/public_html/Lottobets-backend/public/npv/'
 }
