403926751cb01c2eeac339b89211923323dc1350
dhamo7010 <mesh@sixteen87.com>
2023-04-14 18:26:50 +0530

    config changes

--- app/controllers/admin/user.js
+++ app/controllers/admin/user.js
@@ -22,11 +22,11 @@ module.exports = function(model,config){
                 var next = new Date(dateFormat(new Date(t), "yyyy-mm-dd HH:MM:ss",true));
                 next.setHours(next.getHours() + 2);
                 next = dateFormat(next, "yyyy-mm-dd HH:MM:ss");
-			let t = await sequelize_cngapi.transaction();
+			let tra = await sequelize_cngapi.transaction();
 		    try {
 		    	let sql = "SELECT le.ProfileID AS lottoId,le.ID AS lottoEventId,le.Description,ll.ProfileName,ll.State,ll.Country,ll.RegUsed,ll.StartNum,cl.Id AS CountryId,cl.FlagAbv As countryFlag,ll.colorimage,ll.grayscaleimage,DATE_FORMAT(DATE_ADD(le.DrawTime,INTERVAL (-1 *TimeZone)+2 HOUR),'%Y-%m-%d %H:%i:%s') as DrawTime,ll.TimeZone,cl.Continent, DATE_FORMAT(DATE_ADD(le.CutTime,INTERVAL (-1 *TimeZone)+2 HOUR),'%Y-%m-%d %H:%i:%s') as CutTime FROM " + config.Table.LOTTOLIST + " ll LEFT JOIN " + config.Table.LOTTOEVENT + " le ON  ll.ID=le.ProfileID LEFT JOIN " + config.Table.CUNTRYLIST + " cl ON ll.CountryId=cl.Id WHERE DATE_ADD(le.CutTime,INTERVAL (-1 *TimeZone)+2 HOUR)>='" + current + "' AND DATE_ADD(le.CutTime,INTERVAL (-1 *TimeZone)+2 HOUR)<='" + next + "' AND ll.Enable=1  AND le.IsClosed!=1 GROUP BY le.ProfileID ORDER by DATE_ADD(le.CutTime,INTERVAL (-1 *TimeZone)+2 HOUR) limit 20";
-		        let d = await sequelize_cngapi.query(sql, { transaction: t ,type: sequelize_cngapi.QueryTypes.SELECT})
-				await t.commit(); 
+		        let d = await sequelize_cngapi.query(sql, { transaction: tra ,type: sequelize_cngapi.QueryTypes.SELECT})
+				await tra.commit(); 
 				if (result.length) {
                 //var time = require('time');
                 for(let i=0;i<result.length;i++){
