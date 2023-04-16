390e2db0e0b68fa4ba5b9aee81a8268608abf4b4
dhamo7010 <mesh@sixteen87.com>
2023-04-16 16:01:14 +0530

    homeScreen

--- app/controllers/admin/application.js
+++ app/controllers/admin/application.js
@@ -14,7 +14,7 @@ module.exports = function(model,config){
 			
 			let tra = await sequelize_cngapi.transaction();
 		    try {
-		    	let sql = "SELECT DISTINCT(cl.id) AS id,CONCAT(cl.FlagAbv,'.png') as flag,cl.Country,cl.Continent,cl.FlagAbv FROM " + Sys.Config.Table.CUNTRYLIST + " cl JOIN " + config.Table.LOTTOLIST + " ll ON cl.id=ll.CountryId";
+		    	let sql = "SELECT DISTINCT(cl.id) AS id,CONCAT(cl.FlagAbv,'.png') as flag,cl.Country,cl.Continent,cl.FlagAbv FROM " + config.Table.CUNTRYLIST + " cl JOIN " + config.Table.LOTTOLIST + " ll ON cl.id=ll.CountryId";
 		        let country_result = await sequelize_cngapi.query(sql, { transaction: tra ,type: sequelize_cngapi.QueryTypes.SELECT})
 				await tra.commit(); 
 				
