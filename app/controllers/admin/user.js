0c7d93744cb0ac20ee0e6f02ad0c7345442a7985
dhamo7010 <mesh@sixteen87.com>
2023-04-18 17:48:11 +0530

    login api

--- app/controllers/admin/user.js
+++ app/controllers/admin/user.js
@@ -11,6 +11,7 @@ module.exports = function(model,config){
 			
 		let tra_lucky = await sequelize_luckynumberint.transaction();
         let inputs = request.body;
+        console.log("Login==",inputs)
             try {
                 	let sql = "SELECT userId,fullName,photo,mobile,countryCode,deviceId,userName,mobile_ip,mobile_device_id,token,mobile_ip FROM " + config.Table.USER + " WHERE userName=" + sequelize_luckynumberint.escape(inputs.username) + " AND pin=" + sequelize_luckynumberint.escape(inputs.password) + " ORDER BY created_at DESC limit 1";
                     let result = await sequelize_luckynumberint.query(sql, { transaction: tra_lucky ,type: sequelize_luckynumberint.QueryTypes.SELECT})
