1b54f11f3671b8398a7108243927299b55e436d7
dhamo7010 <mesh@sixteen87.com>
2023-04-20 12:43:24 +0530

    walletId

--- app/controllers/admin/user.js
+++ app/controllers/admin/user.js
@@ -18,7 +18,7 @@ module.exports = function(model,config){
         console.log("Login==",inputs);
         let ip = request.connection.remoteAddress.replace(/^.*:/, '');
             try {
-                	let sql = "SELECT userId,fullName,photo,mobile,countryCode,deviceId,userName,mobile_ip,mobile_device_id,token,mobile_ip FROM " + config.Table.USER + " WHERE userName=" + sequelize_luckynumberint.escape(inputs.username) + " AND pin=" + sequelize_luckynumberint.escape(inputs.password) + " AND platform='lottobets' ORDER BY created_at DESC limit 1";
+                	let sql = "SELECT userId,fullName,photo,mobile,countryCode,deviceId,userName,mobile_ip,mobile_device_id,token,mobile_ip,walletId FROM " + config.Table.USER + " WHERE userName=" + sequelize_luckynumberint.escape(inputs.username) + " AND pin=" + sequelize_luckynumberint.escape(inputs.password) + " AND platform='lottobets' ORDER BY created_at DESC limit 1";
                     let result = await sequelize_luckynumberint.query(sql, { transaction: tra_lucky ,type: sequelize_luckynumberint.QueryTypes.SELECT})
                 	 
                 	if (result.length) {
