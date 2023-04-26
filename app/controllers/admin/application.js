31edb1b4aab20f98c664e326fc8eead5758a4eef
dhamo7010 <mesh@sixteen87.com>
2023-04-26 11:28:54 +0530

    countryWiseLottoList

--- app/controllers/admin/application.js
+++ app/controllers/admin/application.js
@@ -418,7 +418,7 @@ module.exports = function(model,config){
 
                 
                 let final_arrList = [];
-                let country = inputs.id ;
+                let country = inputs.countryId ;
                 let Lottolist = "select ID,ProfileName,Country,CountryId,State,DrawTime,CutTime,RegUsed,BonusUsed,LastCreateDate,UpdateTime,TimeZone,StartNum,colorimage,grayscaleimage,Continent from " + config.Table.LOTTOLIST + " where Enable=1 AND  CountryId = '" + country + "'";
                 let result_lottolist = await sequelize_cngapi.query(Lottolist, { transaction: tra_cngapi ,type: sequelize_cngapi.QueryTypes.SELECT});
                     
