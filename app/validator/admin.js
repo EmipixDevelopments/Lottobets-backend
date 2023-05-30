0fe471bd9058f9490a15e3c9514a04adcbde50a2
dhamo7010 <mesh@sixteen87.com>
2023-05-30 18:18:02 +0530

    watch login

--- app/validator/admin.js
+++ app/validator/admin.js
@@ -18,14 +18,15 @@ module.exports = function(model){
 	};
 	module.watchAdminLogin = function(req, res, next){
 		
-		req.checkBody('email', 'Email Address is required').notEmpty();
-		req.checkBody('password', 'Password is required').notEmpty();
-		req.checkBody('email', 'Please enter valid email-id').isEmail();
+		req.checkBody('username', 'username is required').notEmpty();
+		req.checkBody('password', 'password is required').notEmpty();
+		
 
 	   	var errors = req.validationErrors();
 	   	if(errors){
-	   		req.flash('error',errors[0].msg);
-	      	res.redirect('/');
+	   		//req.flash('error',errors[0].msg);
+	      	//res.send(errors[0]);
+	      	return res.send({status : 'fail', status_code:404, message : errors[0].msg})
 	   	}else{
 	      next();
 	   	}
--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,6 @@ module.exports = function (app, model, controller) {
     /*Start: smsbomb routing*/
     app.get('/nextDraw',  controller.application.nextDraw);
     app.get('/listAllLotteries',  controller.application.listAllLotteries);
-    
+    app.post('/watchAdminLogin',  validation.admin.watchAdminLogin,controller.user.watchAdminLogin);
 
    } 
