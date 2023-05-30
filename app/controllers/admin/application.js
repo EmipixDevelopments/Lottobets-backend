5bb0403245e96833878b99f4585ff4a582839061
dhamo7010 <mesh@sixteen87.com>
2023-05-30 13:01:07 +0530

    nextDraw changes

--- app/controllers/admin/application.js
+++ app/controllers/admin/application.js
@@ -41,10 +41,15 @@ module.exports = function(model,config){
                     result[i]['countryFlag'] = config.baseUrl+'/flags/'+result[i].countryFlag+'.png';
                     result[i]['colorimage'] = config.lotto_img_url+'/'+result[i].colorimage;
 
-                     sql = "SELECT DATE_FORMAT(DATE_ADD(DrawTime,INTERVAL (-1 *TimeZone)+2 HOUR),'%Y-%m-%d %H:%i:%s') as DrawTime,Result FROM " + config.Table.LOTTOEVENT + " WHERE ProfileID='" + result[i].ProfileID + "' AND Result!='' ORDER BY DrawTime DESC limit 1";
+                     sql = "SELECT DrawTime,Result FROM " + config.Table.LOTTOEVENT + " WHERE ProfileID='" + result[i].ProfileID + "' AND Result!='' ORDER BY DrawTime DESC limit 1";
                      let lottoevent_result = await sequelize_cngapi.query(sql, { transaction: tra ,type: sequelize_cngapi.QueryTypes.SELECT});
                      if(lottoevent_result.length){
-                        result[i]['lastDrawTime'] = lottoevent_result[0].DrawTime;
+                        var now = dateFormat(new Date(result[0]['DrawTime']), "yyyy-mm-dd HH:MM:ss");
+                        now  = new Date(now);
+                    
+                        var DrawTime = new Date(now.getTime() + (timediff * 1000 * 60 * 60));
+                        DrawTime = dateFormat(DrawTime, "yyyy-mm-dd HH:MM:ss");
+                        result[i]['lastDrawTime'] = DrawTime;
                         result[i]['lastResult'] = lottoevent_result[0].Result;
                      }else{
                         result[i]['lastDrawTime'] = '';
