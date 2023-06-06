675997cf1df50b7c05f7c8776dbe3d21f6a2939c
dhamo7010 <mesh@sixteen87.com>
2023-06-06 17:12:59 +0530

    request time out

--- app/controllers/admin/applicationWatch.js
+++ app/controllers/admin/applicationWatch.js
@@ -36,7 +36,7 @@ module.exports = function(model,config){
                 let profileID = [];
                 for(let i=0;i<result.length;i++){
                     let profileTimezone = result[i]['TimeZone'];
-                    profileID.push(result[i].ProfileID);
+                    profileID.push(result[i][ProfileID]);
                     let timediff = (+2) - (profileTimezone);
                     var now = dateFormat(new Date(result[i]['CutTime']), "yyyy-mm-dd HH:MM:ss");
                     now  = new Date(now);
