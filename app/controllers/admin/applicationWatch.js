64ea38b27caa4e8da0ae1a9624f528af4cd33a8f
dhamo7010 <mesh@sixteen87.com>
2023-06-07 16:42:41 +0530

    next draw issue

--- app/controllers/admin/applicationWatch.js
+++ app/controllers/admin/applicationWatch.js
@@ -48,12 +48,12 @@ module.exports = function(model,config){
                 if(profileIDArr.length){
                     
                 /*result.sort(custom_sort);*/
-                    console.log("profileID=",profileIDArr)
+                    //console.log("profileID=",profileIDArr)
                     profileIDArr = profileIDArr.filter((value, index, array) => array.indexOf(value) === index);
                     var ids = profileIDArr,
                     formatted = `(${ids.map(v => JSON.stringify(v.toString())).join(', ')})`;
 
-                console.log("formatted",formatted)
+                //console.log("formatted",formatted)
 
                     //let next_sql="SELECT le.ProfileID,le.ID,DATE_FORMAT(DATE_ADD(le.DrawTime,INTERVAL (-1 *TimeZone)+2 HOUR),'%Y-%m-%d %H:%i:%s') as DrawTime,le.DrawTime as Draw,ll.TimeZone, le.Result FROM lottolist ll LEFT JOIN lottoevent le ON  ll.ID=le.ProfileID WHERE le.ProfileID IN"+formatted+" AND le.Result!='' AND le.IsClosed=1  ORDER BY le.DrawTime DESC ";
                     let next_sql="SELECT le.ProfileID AS lottoId,le.ID AS lottoEventId,le.Description,ll.ProfileName,ll.State,ll.Country,ll.drawLink,ll.RegUsed,ll.StartNum,ll.live_url,cl.Id AS CountryId,cl.FlagAbv As countryFlag,ll.colorimage,ll.grayscaleimage,DATE_FORMAT(DATE_ADD(le.DrawTime,INTERVAL (-1 *TimeZone)+2 HOUR),'%Y-%m-%d %H:%i:%s') as DrawTime,ll.TimeZone,cl.Continent, DATE_FORMAT(DATE_ADD(le.CutTime,INTERVAL (-1 *TimeZone)+2 HOUR),'%Y-%m-%d %H:%i:%s') as CutTime FROM " + config.Table.LOTTOLIST + " ll LEFT JOIN " + config.Table.LOTTOEVENT + " le ON  ll.ID=le.ProfileID LEFT JOIN " + config.Table.CUNTRYLIST + " cl ON ll.CountryId=cl.Id WHERE DATE_ADD(le.CutTime,INTERVAL (-1 *TimeZone)+2 HOUR)>='" + current + "' AND le.Result='' AND ll.Enable=1  AND le.IsClosed!=1 AND le.ProfileID IN"+formatted+" GROUP BY le.ProfileID ORDER by le.DrawTime DESC ";
@@ -68,12 +68,12 @@ module.exports = function(model,config){
                                     let timediff = (+2) - (profileTimezone);
                                     var now = dateFormat(new Date(result[i]['CutTime']), "yyyy-mm-dd HH:MM:ss");
                                     now  = new Date(now);
-                                    console.log("now1",now);
+                                    //console.log("now1",now);
                                     
                                     var CutTime = new Date(now.getTime() + (timediff * 1000 * 60 * 60));
 
                                     CutTime = dateFormat(CutTime, "yyyy-mm-dd HH:MM:ss");
-                                    console.log("now2",CutTime);
+                                    //console.log("now2",CutTime);
                                     filter_result[j]['CutTime'] = CutTime;
                                     filter_result[j]['countryFlag'] = config.baseUrl+'/flags/'+filter_result[j].countryFlag+'.png';
                                     filter_result[j]['colorimage'] = config.lotto_img_url+'/'+filter_result[j].colorimage;
