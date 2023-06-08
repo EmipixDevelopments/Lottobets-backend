e1ae3a7be2c49ff7aabb262eb88809e7e010bdef
dhamo7010 <mesh@sixteen87.com>
2023-06-08 16:21:56 +0530

    sort issue

--- app/controllers/admin/applicationWatch.js
+++ app/controllers/admin/applicationWatch.js
@@ -93,7 +93,7 @@ module.exports = function(model,config){
                                     
                                     //var DrawTime = new Date(now.getTime() + (timediff * 1000 * 60 * 60));
                                     //DrawTime = dateFormat(DrawTime, "yyyy-mm-dd HH:MM:ss");
-                                    //filter_result[j]['DrawTime'] = dateFormat(filter_result[j].DrawTime, "yyyy-mm-dd HH:MM:ss");
+                                    filter_result[j]['DrawTime'] = dateFormat(filter_result[j].DrawTime, "yyyy-mm-dd HH:MM:ss");
                                     filter_result[j]['lastID'] = result[i].ID;
                                     dataArr.push(filter_result[j])
                                     
