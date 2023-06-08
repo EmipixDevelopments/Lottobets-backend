1587d511fe4788ff20b0a34b13948a396a93f2bb
dhamo7010 <mesh@sixteen87.com>
2023-06-08 10:51:41 +0530

    sorting issue

--- app/controllers/admin/applicationWatch.js
+++ app/controllers/admin/applicationWatch.js
@@ -95,33 +95,12 @@ module.exports = function(model,config){
                 }
                 
 
-                //dataArr.sort(custom_sort);
-
-                var array1 = [];
-                var array2 = [];
-
-                for (const date in dataArr) {
-                  if (dataArr[date].lastUpdateTime) {
-                    array1.push(dataArr[date]);
-                  } else {
-                   array2.push(dataArr[date]);
-                  }
-                }
-
-                var sorted1 = array1.sort(function(a,b) {
-                      return (a.lastUpdateTime > b.lastUpdateTime) ? 1 : -1
-                   }); 
-                   
-                var sorted2 = array2.sort(function(a,b) {
-                      return (a.DrawTime > b.DrawTime) ? 1 : -1
-                   }); 
-                   
-                const sorted = sorted1.concat(sorted2);
+                dataArr.sort(custom_sort);
                 
                 await tra.commit();
                 return response.send({
                     status: "success",
-                    result: sorted,
+                    result: dataArr,
                     message: "Lotto found successfully",
                     status_code: 200
                 });
